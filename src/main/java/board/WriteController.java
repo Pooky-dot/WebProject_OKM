aed20a6c7f6e0ef71a445da065a64cf051e39f61
ogm1325 <ogm1325@naver.com>
2024-11-20 14:20:38 +0900

    중간점검

--- src/main/java/board/WriteController.java
+++ src/main/java/board/WriteController.java
@@ -3,17 +3,19 @@ package board;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import utils.JSFunction;
 
+
 public class WriteController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         // 로그인 확인
         HttpSession session = req.getSession();
